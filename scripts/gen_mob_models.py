35e7104f3889ab522e2204b48121863c65fd140f
lucas.lv <lvjinchi404@outlook.com>
2023-09-03 10:03:59 +0800

    gen mob_models

--- scripts/gen_mob_models.py
+++ scripts/gen_mob_models.py
@@ -1,20 +1,20 @@
 import pandas as pd
 
 # 读取csv文件
-df = pd.read_csv('scripts/models.csv')
+df = pd.read_csv('models.csv')
 
 # 筛选出dtype为mob的数据
 df_mob = df[df['dtype'] == 'mob']
 
 # 将筛选后的数据写入新的csv文件
-df_mob.to_csv('scripts/mob_models.csv', index=False)
+df_mob.to_csv('mob_models.csv', index=False)
 
 # 读取mob_models.csv文件
-df_mob = pd.read_csv('scripts/mob_models.csv')
+df_mob = pd.read_csv('mob_models.csv')
 
 # 对model字段进行去重，对ver_name和brand字段进行合并
 df_mob_unique = df_mob.groupby('model').agg({'ver_name': ' '.join, 'brand': 'first'}).reset_index()
 
 # 将处理后的数据写入新的csv文件
-df_mob_unique.to_csv('scripts/mob_models_unique.csv', index=False)
+df_mob_unique.to_csv('mob_models_unique.csv', index=False)
 
